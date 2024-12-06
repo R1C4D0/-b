ca5f53db3bb9e5c8f3d813bf03547ab31467b305
R1C4D0 <2265117944@qq.com>
2024-12-07 00:05:26 +0800

    Refactoring the RepositoryDirectory enum
    Signed-off-by: R1C4D0 <2265117944@qq.com>

--- proj2/gitlet/domain/repository/RepositoryRepository.java
+++ proj2/gitlet/domain/repository/RepositoryRepository.java
@@ -2,8 +2,59 @@ package gitlet.domain.repository;
 
 import gitlet.domain.model.Repository;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static gitlet.infrastructure.Utils.Utils.join;
+
 public interface RepositoryRepository {
+
+    /**
+     * @return a collection of all the subdirectories in the .gitlet directory.
+     */
+    default Collection<File> getRepoSubdirectories() {
+        return Arrays.stream(RepositoryDirectory.values())
+                .map(dir -> new File(String.join(getRepositoryDirectory().getAbsolutePath(), dir.getDirName())))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * @return the location where the repository is persisted.
+     */
+    default File getRepoLocation() {
+        return join(Repository.GITLET_DIR, "repository");
+    }
+
     void save(Repository repository);
+
     Repository find();
+
     void delete();
+
+    default File getRepositoryDirectory() {
+        return new File(".gitlet");
+    }
+
+    /**
+     * Enum representing the directories in the .gitlet directory.
+     */
+    enum RepositoryDirectory {
+        STAGING("staging"),
+        COMMITS("commits"),
+        BRANCHES("branches"),
+        HEADS("heads"),
+        BLOBS("blobs");
+
+        private final String dirName;
+
+        RepositoryDirectory(String dirName) {
+            this.dirName = dirName;
+        }
+
+        public String getDirName() {
+            return dirName;
+        }
+    }
 }
--- proj2/gitlet/domain/service/Impl/RepositoryDomainServiceImpl.java
+++ proj2/gitlet/domain/service/Impl/RepositoryDomainServiceImpl.java
@@ -1,19 +1,54 @@
 package gitlet.domain.service.Impl;
 
+import gitlet.domain.factory.RepositoryFactory;
 import gitlet.domain.model.Repository;
 import gitlet.domain.service.RepositoryDomainService;
+import gitlet.enums.ErrorMessage;
+import gitlet.infrastructure.Exception.GitletException;
+import gitlet.infrastructure.Utils.Utils;
+import gitlet.infrastructure.repositoryImpl.RepositoryRepositoryImpl;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class RepositoryDomainServiceImpl implements RepositoryDomainService {
-    @Override
-    public boolean repoExists() {
-//        check if the repository exists:
-        return Repository.GITLET_DIR.exists();
-    }
 
     @Override
-    public void initRepository() {
-//        create the .gitlet directory and the initial commit:
-        Repository.GITLET_DIR.mkdir();
-        Repository.INITIAL_COMMIT.save();
+    public Repository initRepository() throws GitletException {
+        if (repoExists()) {
+            throw new GitletException(ErrorMessage.INIT_REPEATED);
+        }
+        try {
+            createRepoDirectories();
+        } catch (IOException e) {
+            throw new GitletException(ErrorMessage.INIT_FAILED);
+        }
+
+        return RepositoryFactory.createInitialRepository();
     }
+
+    /**
+     * Creates all necessary directories for the repository.
+     *
+     * @throws IOException if the directories cannot be created.
+     */
+    private void createRepoDirectories() throws IOException {
+        try {
+            if (!Repository.GITLET_DIR.mkdir()) {
+                throw new IOException();
+            }
+            // After
+            for (File dir
+                    : new RepositoryRepositoryImpl().getRepoSubdirectories()) {
+                Path dirPath = Utils.join(Repository.GITLET_DIR, dir.getName()).toPath();
+                Files.createDirectory(dirPath);
+            }
+        } catch (Exception e) {
+            throw new IOException(ErrorMessage.INIT_FAILED.getMessage());
+        }
+    }
+
+
 }
